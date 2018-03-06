91aed08e7628ae2f2bba944c1c93e9910fb5588a
kenny <411316753@qq.com>
2018-03-06 13:11:31 +0800

    add kenny branch and modify some comments

--- SpringDemo/src/main/java/com/kenny/springframework/event/MessageEvent.java
+++ SpringDemo/src/main/java/com/kenny/springframework/event/MessageEvent.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationEvent;
 
 /**
  * @param :
- * @author: 62083 【makai@casco.com.cn】
+ * @author: kenny [411316753@qq.com]
  * @Date: 2018/3/6-10:47
  * @Description:
  * @return:
