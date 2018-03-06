b5a6712ddc20ce3bb35a57fa97589b75dece2e26
kenny <411316753@qq.com>
2018-03-06 18:14:49 +0800

    add Aware Module

---
+++ SpringDemo/src/main/java/com/kenny/springframework/aware/AwareConfig.java
@@ -0,0 +1,16 @@
+package com.kenny.springframework.aware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @param :
+ * @author: 62083 【makai@casco.com.cn】
+ * @Date: 2018/3/6-16:18
+ * @Description:
+ * @return:
+ */
+@Configuration
+@ComponentScan("com.kenny.springframework.aware")
+public class AwareConfig {
+}
--- SpringDemo/src/main/java/com/kenny/springframework/aware/AwareService.java
+++ SpringDemo/src/main/java/com/kenny/springframework/aware/AwareService.java
@@ -1,8 +1,13 @@
 package com.kenny.springframework.aware;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * @param :
@@ -11,14 +16,29 @@ import org.springframework.core.io.ResourceLoader;
  * @Description:
  * @return:
  */
+@Service
 public class AwareService implements BeanNameAware,ResourceLoaderAware {
+
+  private String beanName;
+  private ResourceLoader loader;
+
   @Override
   public void setBeanName(String s) {
-
+      this.beanName =s;
   }
 
   @Override
   public void setResourceLoader(ResourceLoader resourceLoader) {
+      this.loader = resourceLoader;
+  }
 
+  public  void outputResult(){
+    System.out.println("Bean的名称为: "+ beanName);
+    Resource resource = loader.getResource("classpath:test.ini");
+    try {
+      System.err.println("resource 加载的文件内容是:"+ IOUtils.toString(resource.getInputStream()));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
