8343afbcd88b104e86b0a8a3b1e3579d2581c9c9
kenny <411316753@qq.com>
2018-03-06 11:10:25 +0800

    create the project JavaProject by master kenny

---
+++ SpringDemo/src/main/java/com/kenny/springframework/event/MessageEvent.java
@@ -0,0 +1,11 @@
+package com.kenny.springframework.event;
+
+/**
+ * @param :
+ * @author: 62083 【makai@casco.com.cn】
+ * @Date: 2018/3/6-10:47
+ * @Description:
+ * @return:
+ */
+public class MessageEvent extends ApplicationEvent {
+}
